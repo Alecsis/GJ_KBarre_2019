84093e4a238b5a79865df699d9f595c66126ac33
fabre_ale <alexis.fabre@supelec.fr>
2019-03-15 22:09:37 +0100

    Player input left right

--- src/scenes/SceneChoiceBase.lua
+++ src/scenes/SceneChoiceBase.lua
@@ -15,10 +15,26 @@ local function update(self, dt)
         end
     end
 
+    ---- Player movement ----
     local gravity = 240 * dt
+
+    -- keep Y velocity
+    self.player:setVelocity(0, self.player.vel.y)
+
+    -- keyboard input
+    local isDown = love.keyboard.isDown
+    local vx = 0
+    local vel = 100
+    if isDown('left') then self.player:accelerate(-vel, 0) end
+    if isDown('right') then self.player:accelerate(vel, 0) end
+
+    -- accelerate
     self.player:accelerate(0, gravity)
+
+    -- update position
     local vx, vy = self.player:getVelocity()
     self.player:move(vx * dt, vy * dt)
+
 end
 
 local function draw(self)
@@ -28,58 +44,106 @@ local function draw(self)
     if self.playerSide == "left" then
         self.leftBkg.color[4] = 0.4
         love.graphics.setColor(self.leftBkg.color)
-        love.graphics.rectangle("fill", self.leftBkg.x, self.leftBkg.y, self.leftBkg.width, self.leftBkg.height)
+        love.graphics.rectangle(
+            "fill",
+            self.leftBkg.x,
+            self.leftBkg.y,
+            self.leftBkg.width,
+            self.leftBkg.height
+        )
 
         self.leftBkg.color[4] = 0.8
         love.graphics.setColor(self.leftBkg.color)
-        love.graphics.rectangle("line", self.leftBkg.x, self.leftBkg.y, self.leftBkg.width, self.leftBkg.height)
+        love.graphics.rectangle(
+            "line",
+            self.leftBkg.x,
+            self.leftBkg.y,
+            self.leftBkg.width,
+            self.leftBkg.height
+        )
     else
         self.leftBkg.color[4] = 0.15
         love.graphics.setColor(self.leftBkg.color)
-        love.graphics.rectangle("fill", self.leftBkg.x, self.leftBkg.y, self.leftBkg.width, self.leftBkg.height)
+        love.graphics.rectangle(
+            "fill",
+            self.leftBkg.x,
+            self.leftBkg.y,
+            self.leftBkg.width,
+            self.leftBkg.height
+        )
 
         self.leftBkg.color[4] = 0.3
         love.graphics.setColor(self.leftBkg.color)
-        love.graphics.rectangle("line", self.leftBkg.x, self.leftBkg.y, self.leftBkg.width, self.leftBkg.height)
+        love.graphics.rectangle(
+            "line",
+            self.leftBkg.x,
+            self.leftBkg.y,
+            self.leftBkg.width,
+            self.leftBkg.height
+        )
     end
     -- draw right rectangle
     if self.playerSide == "right" then
         self.rightBkg.color[4] = 0.4
         love.graphics.setColor(self.rightBkg.color)
-        love.graphics.rectangle("fill", self.rightBkg.x, self.rightBkg.y, self.rightBkg.width, self.rightBkg.height)
+        love.graphics.rectangle(
+            "fill",
+            self.rightBkg.x,
+            self.rightBkg.y,
+            self.rightBkg.width,
+            self.rightBkg.height
+        )
 
         self.rightBkg.color[4] = 0.8
         love.graphics.setColor(self.rightBkg.color)
-        love.graphics.rectangle("line", self.rightBkg.x, self.rightBkg.y, self.rightBkg.width, self.rightBkg.height)
+        love.graphics.rectangle(
+            "line",
+            self.rightBkg.x,
+            self.rightBkg.y,
+            self.rightBkg.width,
+            self.rightBkg.height
+        )
     else
         self.rightBkg.color[4] = 0.15
         love.graphics.setColor(self.rightBkg.color)
-        love.graphics.rectangle("fill", self.rightBkg.x, self.rightBkg.y, self.rightBkg.width, self.rightBkg.height)
+        love.graphics.rectangle(
+            "fill",
+            self.rightBkg.x,
+            self.rightBkg.y,
+            self.rightBkg.width,
+            self.rightBkg.height
+        )
 
         self.rightBkg.color[4] = 0.3
         love.graphics.setColor(self.rightBkg.color)
-        love.graphics.rectangle("line", self.rightBkg.x, self.rightBkg.y, self.rightBkg.width, self.rightBkg.height)
+        love.graphics.rectangle(
+            "line",
+            self.rightBkg.x,
+            self.rightBkg.y,
+            self.rightBkg.width,
+            self.rightBkg.height
+        )
     end
 
     -- reset colors
     love.graphics.setColor(1, 1, 1)
 
     -- draw platform
-    love.graphics.draw(self.platform.sprite, self.platform.x, self.platform.y, 0, self.platform.scale)
+    love.graphics.draw(
+        self.platform.sprite,
+        self.platform.x,
+        self.platform.y,
+        0,
+        self.platform.scale
+    )
 
     -- draw player
     self.player:draw()
 end
 
-local function choiceChanged(self)
-    print("Player choice changed: " .. self.playerSide)
-end
+local function choiceChanged(self) print("Player choice changed: " .. self.playerSide) end
 
-local function keyPressed(self, k)
-    if k == "escape" then
-        self.manager:load("start")
-    end
-end
+local function keyPressed(self, k) if k == "escape" then self.manager:load("start") end end
 
 local function SceneChoiceBase(pSceneManager)
     local SceneBase = require("lib.SceneBase")
@@ -90,25 +154,30 @@ local function SceneChoiceBase(pSceneManager)
     self.height = love.graphics.getHeight()
 
     ----- objects -----
-    self.platform = { x=0, y=0, width=480, height=160 }
+    self.platform = {x = 0, y = 0, width = 480, height = 160}
     self.platform.x = (self.width - self.platform.width) / 2
     self.platform.y = self.height - self.platform.height
     self.platform.sprite = love.graphics.newImage("assets/platform.png")
     self.platform.scale = self.platform.width / self.platform.sprite:getWidth()
 
-    self.leftBkg = { x=0, y=0, width=self.width / 2, height=self.height }
-    self.leftBkg.color = { 1, 0, 0, 0 }
+    self.leftBkg = {x = 0, y = 0, width = self.width / 2, height = self.height}
+    self.leftBkg.color = {1, 0, 0, 0}
 
-    self.rightBkg = { x=self.width / 2, y=0, width=self.width / 2, height=self.height }
-    self.rightBkg.color = { 0, 0, 1, 0 }
+    self.rightBkg = {
+        x = self.width / 2,
+        y = 0,
+        width = self.width / 2,
+        height = self.height
+    }
+    self.rightBkg.color = {0, 0, 1, 0}
 
-     ----- player ----    
+    ----- player ----    
     local player = require("src.objects.Player")()
     player:setPosition(self.width / 2 - 50, self.height / 2)
     player:setVelocity(100, -100)
     self.player = player
     self.playerSide = "left"
-    
+
     ----- interface functions ----
     self.update = update
     self.draw = draw
